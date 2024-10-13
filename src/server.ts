ebb7c3747d1bd983013ae94a5d8d43a00dc3bbeb
Evgeny Kniazev <eugene.kniazev@gmail.com>
2024-10-13 21:38:42 +0200

    feat: run simple koa server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,37 @@
+import Koa from "koa";
+import Router from "koa-router";
+import bodyParser from "koa-bodyparser";
+import dotenv from "dotenv";
+
 void (async () => {
   console.log("Starting server");
+
+  dotenv.config();
+
+  const app = new Koa();
+  const router = new Router();
+
+  // Middleware to parse JSON and text/plain
+  app.use(
+    bodyParser({
+      enableTypes: ["json", "text"],
+      extendTypes: {
+        text: ["text/plain"],
+      },
+    })
+  );
+
+  // TODO: Routes
+
+  app.use(router.routes()).use(router.allowedMethods());
+
+  // Error Handling Middleware
+  app.on("error", (err, ctx) => {
+    console.error("Server error", err, ctx);
+  });
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 })();
